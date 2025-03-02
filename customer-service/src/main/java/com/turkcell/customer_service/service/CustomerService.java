b8a694ccf76139c3a32ae5835a027f374e1516cc
merve-kayali <mervekyl1438@gmail.com>
2025-03-02 21:05:15 +0300

    ADD: Added customer service and BusinessException

---
+++ customer-service/src/main/java/com/turkcell/customer_service/service/CustomerService.java
@@ -0,0 +1,15 @@
+package com.turkcell.customer_service.service;
+
+import com.turkcell.customer_service.dto.CustomerRequest;
+import com.turkcell.customer_service.dto.CustomerResponse;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CustomerService {
+    CustomerResponse createCustomer(CustomerRequest request);
+    CustomerResponse getCustomerByEmail(String email);
+    List<CustomerResponse> getAllCustomers();
+    CustomerResponse updateCustomer(UUID id,CustomerRequest request);
+    void deleteCustomer(UUID id);
+}
