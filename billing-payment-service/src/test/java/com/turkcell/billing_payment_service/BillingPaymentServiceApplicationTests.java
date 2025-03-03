4dc09ea7a2062a622a5f0cd409664e17d56db2a0
Çınar Berke Ergül <berkeergul2000@gmail.com>
2025-03-03 08:59:38 +0300

    billing-payment-service

---
+++ billing-payment-service/src/test/java/com/turkcell/billing_payment_service/BillingPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.billing_payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
