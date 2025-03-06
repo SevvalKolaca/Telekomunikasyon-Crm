b84c09d53ea31dc971e611febdc1bf58822fa152
Çınar Berke Ergül <berkeergul2000@gmail.com>
2025-03-06 15:22:39 +0300

    deploy v2

--- turkcell-t2/target/apidocs/package-search-index.js
+++ turkcell-t2/target/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.ergulberke"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.ergulberke"},{"l":"io.github.ergulberke.exception"}];updateSearchResults();
