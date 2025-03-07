fcecab9fc33f44153c1cedaa6b4ab2ff3d37b2f5
Çınar Berke Ergül <berkeergul2000@gmail.com>
2025-03-07 12:05:21 +0300

    bill notification baglantisi

--- turkcell-t2/src/main/java/io/github/ergulberke/event/billingPayment/BillCreatedEvent.java
+++ turkcell-t2/src/main/java/io/github/ergulberke/event/billingPayment/BillCreatedEvent.java
@@ -2,6 +2,8 @@ package io.github.ergulberke.event.billingPayment;
 
 import java.time.LocalDate;
 
+
+
 public class BillCreatedEvent {
 
     private Long id;
@@ -19,4 +21,45 @@ public class BillCreatedEvent {
     private Long customerId;
 
 
+    public BillCreatedEvent(Long id, String billNumber, Double amount, LocalDate dueDate, LocalDate paymentDate, String status, Long customerId) {
+        this.id = id;
+        this.billNumber = billNumber;
+        this.amount = amount;
+        this.dueDate = dueDate;
+        this.paymentDate = paymentDate;
+        this.status = status;
+        this.customerId = customerId;
+    }
+
+    public BillCreatedEvent() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getBillNumber() {
+        return billNumber;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public LocalDate getPaymentDate() {
+        return paymentDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
 }
--- turkcell-t2/target/apidocs/package-search-index.js
+++ turkcell-t2/target/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.ergulberke"},{"l":"io.github.ergulberke.exception"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.ergulberke"},{"l":"io.github.ergulberke.event.billingPayment"},{"l":"io.github.ergulberke.exception"}];updateSearchResults();
