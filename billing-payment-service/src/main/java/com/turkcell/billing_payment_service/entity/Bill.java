d29540c45f1116e4838ba2f6adad38d81c71ecf6
Çınar Berke Ergül <berkeergul2000@gmail.com>
2025-03-19 09:41:47 +0300

    update

--- billing-payment-service/src/main/java/com/turkcell/billing_payment_service/entity/Bill.java
+++ billing-payment-service/src/main/java/com/turkcell/billing_payment_service/entity/Bill.java
@@ -1,10 +1,6 @@
 package com.turkcell.billing_payment_service.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDate;
@@ -12,6 +8,7 @@ import java.time.LocalDate;
 @Entity
 @Getter
 @Setter
+@Table(name = "bills")
 public class Bill {
     
     @Id
