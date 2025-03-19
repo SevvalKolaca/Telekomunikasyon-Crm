9a2b5a5b32c0fd469bc326758961323d3cea8652
duyguhalisyama1 <duyguhalisyama@gmail.com>
2025-03-19 20:07:05 +0300

    config servera bağımlılık ekledim servislere eklenmesi gerekiyor. docker-compose.yml dosyasına prometheus-alertmanager-grafana-zipkin eklendi.

--- plan-service/src/main/java/com/turkcell/plan_service/client/BillingServiceClient.java
+++ plan-service/src/main/java/com/turkcell/plan_service/client/BillingServiceClient.java
@@ -1,11 +1,11 @@
 package com.turkcell.plan_service.client;
 
+import java.util.UUID;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.UUID;
 
 @FeignClient(name = "billing-service", url = "${spring.services.billing-payment.url}")
 @RequestMapping("/api/v1/billing")
