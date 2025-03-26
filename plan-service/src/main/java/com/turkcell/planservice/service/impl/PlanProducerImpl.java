ed2254fda644f8dd61bf854d90ad8423314bd7bf
Rabia Demiroğ <77547046+rabiademirog@users.noreply.github.com>
2025-03-26 21:46:08 +0300

    Update PlanProducerImpl.java

--- plan-service/src/main/java/com/turkcell/planservice/service/impl/PlanProducerImpl.java
+++ plan-service/src/main/java/com/turkcell/planservice/service/impl/PlanProducerImpl.java
@@ -1,24 +1,35 @@
-package com.turkcell.plan_service.service.impl;
+package com.turkcell.planservice.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.turkcell.planservice.event.PlanCreatedEvent;
+import com.turkcell.planservice.event.PlanUpdatedEvent;
+import com.turkcell.planservice.service.PlanProducer;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
-import com.turkcell.plan_service.service.PlanProducer;
-import io.github.ergulberke.event.plan.PlanCreatedEvent;
 
 @Service
+@RequiredArgsConstructor
 public class PlanProducerImpl implements PlanProducer {
 
-    @Autowired
-    private StreamBridge streamBridge;
+    private final StreamBridge streamBridge;
 
     @Override
     public void sendPlanCreatedEvent(PlanCreatedEvent event) {
         boolean result = streamBridge.send("planCreatedFunction-out-0", event);
         if (result) {
-            System.out.println("Plan Created Event Kafka'ya gönderildi: " + event);
+            System.out.println("✅ Plan Created Event Kafka'ya başarıyla gönderildi: " + event);
         } else {
-            System.err.println("Plan Created Event Kafka'ya gönderilemedi!");
+            System.err.println("❌ Plan Created Event Kafka'ya gönderilemedi!");
+        }
+    }
+
+    @Override
+    public void sendPlanUpdatedEvent(PlanUpdatedEvent event) {
+        boolean result = streamBridge.send("planUpdatedFunction-out-0", event);
+        if (result) {
+            System.out.println("✅ Plan Updated Event Kafka'ya başarıyla gönderildi: " + event);
+        } else {
+            System.err.println("❌ Plan Updated Event Kafka'ya gönderilemedi!");
         }
     }
 }
