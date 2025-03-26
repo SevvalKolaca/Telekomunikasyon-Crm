1a52cca2ff18613ec6b9560c2e0d5bd925cfb2ea
Rabia Demiroğ <77547046+rabiademirog@users.noreply.github.com>
2025-03-26 21:52:31 +0300

    Add files via upload

---
+++ turkcell-t2/src/main/java/io/github/ergulberke/event/plan/PlanUpdatedEvent.java
@@ -0,0 +1,26 @@
+package io.github.ergulberke.event.plan;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlanUpdatedEvent {
+    private UUID id;
+    private String name;
+    private BigDecimal price;
+    private PlanType type;
+    private PlanStatus status;
+    private Integer smsLimit;
+    private Integer internetLimit;
+    private Integer voiceLimit;
+    private LocalDateTime updatedAt;
+}
