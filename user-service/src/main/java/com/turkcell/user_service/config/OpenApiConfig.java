27afa129e674c4a326ca3f4bc72f562fb6f05114
SevvalKolaca <sevval.kolaca.00@gmail.com>
2025-04-14 15:31:15 +0300

    user-service swagger authorize aktif edildi.

--- user-service/src/main/java/com/turkcell/user_service/config/OpenApiConfig.java
+++ user-service/src/main/java/com/turkcell/user_service/config/OpenApiConfig.java
@@ -1,11 +1,15 @@
 package com.turkcell.user_service.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 
 @Configuration
@@ -13,16 +17,25 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
                 .info(new Info()
                         .title("User Service API")
                         .version("1.0")
-                        .description("Telekomunikasyon CRM User Service API Documentation")
+                        .description("Telekomünikasyon CRM User Service API Documentation")
                         .contact(new Contact()
                                 .name("Turkcell")
                                 .email("info@turkcell.com.tr"))
                         .license(new License()
                                 .name("Turkcell License")
-                                .url("https://www.turkcell.com.tr")));
+                                .url("https://www.turkcell.com.tr")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
